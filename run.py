4992c1d9d8fe97bad5d6739776fe895dde853150
rene205 <83230592+rene205@users.noreply.github.com>
2022-06-21 15:07:07 +0200

    Add files via upload

---
+++ run.py
@@ -0,0 +1,10 @@
+from flaskblog import app #, db
+#from flaskblog.models import User, Post
+
+if __name__ == '__main__':
+    app.run()
+
+#User.__table__.drop(db.engine)
+#Post.__table__.drop(db.engine)
+
+#db.create_all()
